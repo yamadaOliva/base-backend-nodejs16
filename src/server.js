d1da1c9fb4ef24cafc4cfbc13b927b5b2006a893
yamadaOliva <vietanh87.forwork@gmail.com>
2023-05-18 02:22:59 +0700

    add ORM

--- src/server.js
+++ src/server.js
@@ -2,16 +2,18 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import initAPI from "./routes/api.js";
 import configCors from "./config/cors.js";
+import connectDB from "./config/connectDB.js";
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 configCors(app);
-
+connectDB();
 initAPI(app);
 
 app.use(cookieParser());
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
